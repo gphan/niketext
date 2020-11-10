885b45b0e06c9b391487a7a533529fa944ee9cbb
Giang Phan <john.phan@gmail.com>
2020-11-09 17:51:05 -0800

    New trained model

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ def index():
 def generate():
     temperature = request.args.get('temperature', default=0.5, type=float)
     samples = request.args.get('samples', default=2, type=int)
-    textgen = textgenrnn("niketext_weights.hdf5")
+    textgen = textgenrnn(weights_path="niketext_weights.hdf5",
+                         vocab_path="niketext_vocab.json",
+                         config_path="niketext_config.json")
     text = textgen.generate(n=samples, temperature=temperature, return_as_list=True)
     return "\n".join(text)
 
