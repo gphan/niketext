2c486773756b4f9d3f8213d72958ae433a64edc9
Giang Phan <john.phan@gmail.com>
2020-11-09 17:01:47 -0800

    Add temperature and sample size inputs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from textgenrnn import textgenrnn
 
 app = Flask(__name__)
@@ -12,8 +12,10 @@ def index():
 
 @app.route('/generate', methods=['GET'])
 def generate():
+    temperature = request.args.get('temperature', default=0.5, type=float)
+    samples = request.args.get('samples', default=2, type=int)
     textgen = textgenrnn("niketext_weights.hdf5")
-    text = textgen.generate(n=1, return_as_list=True)
+    text = textgen.generate(n=samples, temperature=temperature, return_as_list=True)
     return "\n".join(text)
 
 if __name__ == '__main__':
